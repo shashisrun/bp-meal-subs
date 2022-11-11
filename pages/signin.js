895a991ae56c30e6ae4e5475d077ff303318d241
Shashi Sharma <shashi.sharma1@publicissapient.com>
2022-11-11 17:50:08 +0530

    sign page design updated

--- pages/signin.js
+++ pages/signin.js
@@ -64,7 +64,7 @@ export default function Signin() {
 
     return (
         <>
-            <div className='h-screen py-2 relative'>
+            <div className='h-screen py-2'>
                 <div className="w-full carousel rounded-box">
                     <div className="carousel-item w-full">
                         <ImageViewer src={signin1} width={1288} height={1364} classname='w-full' />
@@ -76,7 +76,7 @@ export default function Signin() {
                         <ImageViewer src={signin3} width={1288} height={1364} classname='w-full' />
                     </div>
                 </div>
-                <div className='bg-primary h-full rounded-lg pt-5 text-white -mt-20 -ml-2 z-10 absolute top-50 pb-20'>
+                <div className='bg-primary h-full rounded-lg pt-5 text-white -mt-20 -ml-2 z-10 absolute h-screen'>
                     <div className='px-20 py-2 '>
                         <ImageViewer src={logo} width={2551} height={296} className="w-full" />
                     </div>
