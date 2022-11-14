9a998840fe4147df88647cb8cb6a4a96b462dfa1
Shashi Sharma <shashi.sharma1@publicissapient.com>
2022-11-14 15:30:28 +0530

    offer autoscroll enabled

--- components/offers.js
+++ components/offers.js
@@ -1,7 +1,11 @@
 import Link from "next/link";
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 import ImageViewer from "./imageViewer";
 
 export default function Offers() {
+    const [current, setCurrent] = React.useState(0)
     const offers = [
         {
             src: 'offers/starter_package.png',
@@ -19,17 +23,38 @@ export default function Offers() {
             link: '/completeProfile'
         }
     ]
+
+    const incrementScroll = () => {
+        if (current < offers.length) {
+            setCurrent(current + 1)
+        } else {
+            setCurrent(0)
+        }
+    }
+
+    React.useEffect(() => {
+        const offerdiv = document.getElementById('offers-' + current);
+        if (offerdiv) offerdiv.scrollIntoView();
+        setTimeout(() => {
+            incrementScroll();
+        }, 2000)
+    }, [current])
+
+
+
     return (
         <div className="w-full carousel rounded-box">
-            {offers.map((offer, key) => {
-                return (
-                    <div className="carousel-item w-2/3 px-1" key={key}>
-                        <Link href={offer.link}>
-                            <ImageViewer src={offer.src} className="w-full rounded-box" alt={offer.alt} width={1080} height={1080} />
-                        </Link>
-                    </div>
-                )
-            })}
+            <Carousel autoPlay={true} infiniteLoop={true}>
+                {offers.map((offer, key) => {
+                    return (
+                        <div key={key}>
+                            <Link href={offer.link}>
+                                <ImageViewer src={offer.src} className="w-full rounded-box" alt={offer.alt} width={1080} height={1080} />
+                            </Link>
+                        </div>
+                    )
+                })}
+            </Carousel>
         </div>
     )
 }
